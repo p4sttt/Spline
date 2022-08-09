91fd72e57584640b80cbfc602a59e86c5aafa7f5
da_m1r <72396878+p4sttt@users.noreply.github.com>
2022-08-09 17:11:25 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <>
+      <Spline className='bg' scene="https://prod.spline.design/WdN-0uzG0Z3RLP0a/scene.splinecode" />
+    </>
+  );
+}
